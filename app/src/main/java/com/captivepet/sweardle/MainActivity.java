10905014485ce948a6fc72b88079eb3273fe700c
pmackinney <paul@mackinney.net>
2022-06-26 21:53:10 -0700

    restart refactor1

--- app/src/main/java/com/captivepet/sweardle/MainActivity.java
+++ app/src/main/java/com/captivepet/sweardle/MainActivity.java
@@ -1,7 +1,8 @@
 package com.captivepet.sweardle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Size;
@@ -9,13 +10,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.captivepet.sweardle.ui.main.GameFragment;
 import com.captivepet.sweardle.ui.main.KeyboardFragment;
 
 public class MainActivity extends AppCompatActivity {
     public Size windowSize;
-    GameFragment tile;
+    GameFragment game;
     KeyboardFragment keyboard;
     ViewGroup container;
 
@@ -23,6 +25,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            keyboard = KeyboardFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().replace(
+                    R.id.keyboard_fragment, keyboard).commitNow();
+            game = GameFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().replace(
+                    R.id.game_fragment, game).commitNow();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        GameFragment game = (GameFragment) getSupportFragmentManager().findFragmentById(R.id.game_fragment);
+        KeyboardFragment keyboard = (KeyboardFragment) getSupportFragmentManager().findFragmentById(R.id.keyboard_fragment);
         container = findViewById(R.id.container);
 
         View parent = (View) container.getParent();
@@ -30,20 +47,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onGlobalLayout() {
                 parent.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                game.setSize(getDisplayContentHeight());
                 int gameSize = keyboard.computeSizes(getDisplayContentHeight());
                 keyboard.init();
-                tile.init(gameSize);
+                game.init(gameSize);
             }
         });
+        super.onResume();
+    }
 
-        if (savedInstanceState == null) {
-            keyboard = KeyboardFragment.newInstance();
-            getSupportFragmentManager().beginTransaction().replace(
-                    R.id.keyboard_fragment, keyboard).commitNow();
-            tile = GameFragment.newInstance();
-            getSupportFragmentManager().beginTransaction().replace(
-                    R.id.main_fragment, tile).commitNow();
-        }
+    public void onCharKey(View view) {
+        game.processChar(((Button) view).getText().toString().charAt(0));
+    }
+
+    public void onSpecialKey(View view) {
+        game.processChar(view.getTag().toString().charAt(0));
     }
 
     // https://gist.github.com/dominicthomas/8257203
@@ -54,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
         if (getActionBar() != null) {
             actionBarHeight = getActionBar().getHeight();
         }
-        int contentTop = ((ViewGroup) findViewById(android.R.id.content)).getTop();
+        int contentTop = findViewById(android.R.id.content).getTop();
         windowManager.getDefaultDisplay().getSize(size);
         size.y -= (contentTop + actionBarHeight);
         return size;
